bc7231fd21780f8380f6f906c928a9d60b0288ff
Ecommerwebsite <tahab9044@gmail.com>
2023-11-17 18:33:19 -0800

    Add New Thing

--- corenodule.js
+++ corenodule.js
@@ -1,5 +1,10 @@
 // global modules means import karna ki zaroorat nahi hoti in ko
+
 console.log("The console is built in so its core module");
+console.log(__dirname)  // tell us directory name
+
 // non global modules 
-const fs = require('fs');
-fs.writeFileSync("Note.txt","Automatic create file")
+
+ const fs = require('fs');
+ fs.writeFileSync("Note.txt","Automatic create file")
+
